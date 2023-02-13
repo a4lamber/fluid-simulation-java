071941f839c673c22edcb30b542c41cdef2b6c36
Adam Zhang <adamhimy19@gmail.com>
2023-02-13 11:30:00 -0500

    Java MOOC scripts

---
+++ tutorial/UofHelsinkiJavaMooc/part1/mooc-java-programming-i/part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -0,0 +1,13 @@
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        // Write your program here
+        /*
+        - this is how to comment out a block of code in Java
+        */
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    }
+}
