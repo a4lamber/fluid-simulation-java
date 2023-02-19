173f8afbd3ec7aca7f1889c756e4af91201fb71e
Adam Zhang <adamhimy19@gmail.com>
2023-02-18 22:39:56 -0500

    the advance and clone in oop is kinda tricky

--- tutorial/UofHelsinkiJavaMooc/part1/mooc-java-programming-i/part05-Part05_16.DatingApp/src/main/java/Main.java
+++ tutorial/UofHelsinkiJavaMooc/part1/mooc-java-programming-i/part05-Part05_16.DatingApp/src/main/java/Main.java
@@ -3,5 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         // test your program here!
+        SimpleDate date = new SimpleDate(30, 12, 2011);
+        System.out.println(date);
+
+        date.advance(23);
+        
+        System.out.println(date);
+        
     }
 }
--- tutorial/UofHelsinkiJavaMooc/part1/mooc-java-programming-i/part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ tutorial/UofHelsinkiJavaMooc/part1/mooc-java-programming-i/part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -1,16 +1,69 @@
 
 public class SimpleDate {
-
+    // attributes
     private int day;
     private int month;
     private int year;
 
+    //constructor
     public SimpleDate(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
 
+    // methods
+    
+    public void advance(int howManyDays) {
+        /*
+        move date by one day assuming each month has 30 day
+        also update month and year based on the how date advance
+        */
+        for (int i = 0; i < howManyDays;i++) {
+            advanceHelper();
+        }
+    }
+    
+    // helper
+    public void advanceHelper() {
+        int dayLimit = 30;
+        int monthLimit = 12;
+        
+        this.day += 1;
+        
+        // 如果大于30天则
+        if (this.day > dayLimit) {
+            this.day = 1;
+            
+            // advances month
+            this.month += 1;
+            
+            // advances month by one
+            if (this.month > monthLimit) {
+                // reset month
+                this.month = 1;
+                // advance year
+                this.year += 1;
+            }
+        }
+    }
+    
+    // overloaded method
+    public void advance() {
+        this.advance(1);
+    }
+    
+    
+    // 
+    public SimpleDate afterNumberOfDays(int days) {
+        SimpleDate clone = new SimpleDate(this.day,this.month,this.year);
+        
+        clone.advance(days);
+        
+        return clone;
+    }
+    
+    
     @Override
     public String toString() {
         return this.day + "." + this.month + "." + this.year;
